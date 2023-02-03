ef96ddd7c03d08d4649750581437783617603758
chatreeck7 <chatree.devl@gmail.com>
2023-02-03 16:59:44 +0700

    Update layout and Conform the design to figma !

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -9,34 +9,51 @@ export default function Login() {
     // <div className=' '>
     <div className="container-fluid fill bg-dark">
       <div className="row fill">
-        <div className="col-md-6 d-flex">
-          <h1 className="text-start ms-10 mt-10">Bookbix</h1>
-          <div className="align-self-center">
-            <img
-              src={login_costume}
-              className="img-fluid"
-              alt="costume-of-login-page"
-            />
-          </div>
+        <div className="col-md-6 login-left">
+          {/* <h1 className="text-start d-inline bookbix-logo">Bookbix</h1> */}
+          <img
+            src={login_costume}
+            className="img-fluid"
+            alt="costume-of-login-page"
+          />
         </div>
-        <div className="col-md-6 login-field d-flex flex-column justify-content-center align-items-center">
-          <h1 className="p-10">Sign in to Bookbix</h1>
-          <Form>
-            <Form.Group className="mb-3" controlId="formGroupEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-          </Form>
-          <Button className="rounded-pill" variant="light" type="submit">
-            Login
-          </Button>
+        <div className="login-right col-md-6">
+          <div className="login-header">
+            <h1>Sign in to <span>Bookbix</span></h1>
+          </div>
+          <form className="login-form col-md-8">
+            <div className="login-form-content">
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+                <small id="emailHelp" className="form-text text-muted">
+                  We'll never share your email with anyone else.
+                </small>
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                />
+              </div>
+              <a className='text-end'>forget password?</a>
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
       </div>
+      //{' '}
     </div>
-    // </div>
   )
 }
