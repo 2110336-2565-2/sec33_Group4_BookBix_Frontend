653f8d6a8c315e9f58a1c7940288d2a60e08ff01
Nantawat Rattanaton <nick.rattanaton@gmail.com>
2023-02-04 22:28:17 +0700

    🐱‍👤Setup Login page via react bootstrap

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { Form, Button, Container } from 'react-bootstrap'
 
 export default function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-      <p>Just for Template</p>
-    </div>
+    <Container className="w-4 h-5 p-5">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   )
 }
