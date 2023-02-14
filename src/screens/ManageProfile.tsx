18859bc8a0d6bee9db02b9c47a2a2369d112f60e
Ham <6330104021@student.chula.ac.th>
2023-02-14 21:51:46 +0700

    Add event trigger and connect to backend

--- src/screens/ManageProfile.tsx
+++ src/screens/ManageProfile.tsx
@@ -1,45 +1,113 @@
 import React from 'react'
 import { Button, Form } from 'react-bootstrap'
-import photo from '../assets/images/react.svg'
-// page for update user info firstname, lastname, sex, birthday, email and profile picture
+import '../assets/scss/_manageProfile.scss'
+import { useState } from 'react'
+const url = import.meta.env.VITE_API_URL
+
+// page for update user info firstname, lastname, sex, birthdate, email
 export default function ManageProfile() {
+  const [firstname, setFirstname] = useState<string>('')
+  const [lastname, setLastname] = useState<string>('')
+  const [sex, setSex] = useState<string>('')
+  const [birthdate, setBirthdate] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [error, setError] = useState<string>('')
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(`${url}/manage-profile`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ firstname, lastname, sex, birthdate, email }),
+      })
+      const data = await response.json()
+      if (!response.ok) {
+        setError(data.message)
+        return
+      }
+      // Save the user information in local storage or in the state
+      localStorage.setItem('user', JSON.stringify(data.user))
+
+      // Redirect the user to the homepage
+      window.location.href = '/home'
+    } catch (error) {
+      setError('Something went wrong, please try again later')
+    }
+  }
+
   return (
     <div className="container-fluid text-light fill bg-dark ">
       <div className=" d-flex align-items-center justify-content-center pt-5">
         {/* <img className="col-md-6 img-fluid " src={photo} /> */}
-        <Form className="row col-md-6 profile-manage pb-3 pt-3">
+        <Form
+          className="row col-md-6 profile-manage pb-3 pt-3"
+          onSubmit={handleSubmit}
+        >
           <h1 className="d-flex align-items-center justify-content-center">
             <br></br>
             <span className="text-black fw-bold">Manage</span> Profile
           </h1>
           <Form.Group className="row mb-3 col-md-8 " controlId="formFirstname">
             <Form.Label>Firstname</Form.Label>
-            <Form.Control type="text" placeholder="Enter firstname" />
+            <Form.Control
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setFirstname(e.target.value)
+              }
+              type="text"
+              placeholder="Enter firstname"
+            />
           </Form.Group>
 
           <Form.Group className="row mb-3 col-md-8 " controlId="formLastname">
-            <Form.Label>Firstname</Form.Label>
-            <Form.Control type="text" placeholder="Enter lastname" />
+            <Form.Label>Lirstname</Form.Label>
+            <Form.Control
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setLastname(e.target.value)
+              }
+              type="text"
+              placeholder="Enter lastname"
+            />
           </Form.Group>
 
           <Form.Group className="row mb-3 col-md-8 " controlId="formSex">
             <Form.Label>Sex</Form.Label>
-            <Form.Select placeholder="Enter Sex">
-              <option>Choose...</option>
-              <option>Male</option>
-              <option>Female</option>
-              <option>LGBTQ+</option>
+            <Form.Select
+              onChange={(e) => {
+                setSex(e.target.value)
+              }}
+              aria-label="Default select example"
+              placeholder="Enter Sex"
+            >
+              <option value="">Choose...</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="LGBTQ+">LGBTQ+</option>
             </Form.Select>
           </Form.Group>
 
-          <Form.Group className="row mb-3 col-md-8 " controlId="formBirthday">
-            <Form.Label>Birthday</Form.Label>
-            <Form.Control type="date" placeholder="Enter Birthday" />
+          <Form.Group className="row mb-3 col-md-8 " controlId="formBirthdate">
+            <Form.Label>Birthdate</Form.Label>
+            <Form.Control
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setBirthdate(e.target.value)
+              }
+              type="date"
+              placeholder="Enter Birthdate"
+            />
           </Form.Group>
 
           <Form.Group className="row mb-3 col-md-8 " controlId="formEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
+              type="email"
+              placeholder="Enter email"
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
--- src/screens/provider/ManageLocation.tsx
+++ src/screens/provider/ManageLocation.tsx
@@ -1,19 +1,77 @@
 import React from 'react'
 import { Button, Form } from 'react-bootstrap'
+import '../../assets/scss/_manageProfile.scss'
+import { useState } from 'react'
+const url = import.meta.env.VITE_API_URL
+
+interface Location {
+  locationname: string
+  address: string
+  description: string
+  openTime: string
+  closeTime: string
+  url: string
+  availableDay: Array<string>
+}
 
 export default function ManageLocation() {
+  const [error, setError] = useState<string>('')
+  const [location, setLocation] = useState<Location>({
+    locationname: '',
+    address: '',
+    description: '',
+    openTime: '',
+    closeTime: '',
+    url: '',
+    availableDay: [],
+  })
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch(`${url}/manage-location`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ location }),
+      })
+      const data = await response.json()
+      if (!response.ok) {
+        setError(data.message)
+        return
+      }
+      // Save the user information in local storage or in the state
+      localStorage.setItem('user', JSON.stringify(data.user))
+
+      // Redirect the user to the homepage
+      window.location.href = '/home'
+    } catch (error) {
+      setError('Something went wrong, please try again later')
+    }
+  }
   return (
     <div className="container-fluid text-light fill bg-dark ">
       <div className=" d-flex align-items-center justify-content-center pt-5 ">
         {/* <img className="col-md-6 img-fluid " src={photo} /> */}
-        <Form className="row col-md-6 profile-manage pb-3 pt-3">
+        <Form
+          className="row col-md-6 profile-manage pb-3 pt-3"
+          onSubmit={handleSubmit}
+        >
           <h1 className="d-flex align-items-center justify-content-center">
             <br></br>
             <span className="text-black fw-bold">Manage</span> Location
           </h1>
           <Form.Group className="row mb-3 col-md-8 " controlId="locationName">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter Name" />
+            <Form.Control
+              onChange={(e) => {
+                // setLocationname(e.target.value)
+                setLocation({ ...location, locationname: e.target.value })
+              }}
+              type="text"
+              placeholder="Enter Name"
+            />
           </Form.Group>
 
           <Form.Group
@@ -21,7 +79,14 @@ export default function ManageLocation() {
             controlId="locationDescription"
           >
             <Form.Label>Description</Form.Label>
-            <Form.Control type="text" placeholder="Enter Description" />
+            <Form.Control
+              onChange={(e) => {
+                // setUrl(e.target.value)
+                setLocation({ ...location, description: e.target.value })
+              }}
+              type="text"
+              placeholder="Enter Description"
+            />
           </Form.Group>
 
           <Form.Group
@@ -29,18 +94,38 @@ export default function ManageLocation() {
             controlId="locationAddress"
           >
             <Form.Label>Address</Form.Label>
-            <Form.Control type="text" placeholder="Enter Address" />
+            <Form.Control
+              onChange={(e) => {
+                // setAddress(e.target.value)
+                setLocation({ ...location, address: e.target.value })
+              }}
+              type="text"
+              placeholder="Enter Address"
+            />
+          </Form.Group>
+          <Form.Group className="row mb-3 col-md-8 " controlId="locationURL">
+            <Form.Label>Google Map URL</Form.Label>
+            <Form.Control
+              onChange={(e) => {
+                // setUrl(e.target.value)
+                setLocation({ ...location, url: e.target.value })
+              }}
+              type="text"
+              placeholder="Enter URL"
+            />
           </Form.Group>
           <Form.Group
             className="row mb-3 col-md-8 "
             controlId="locationOpenTime"
           >
             <Form.Label>Open Time</Form.Label>
-            <Form.Control type="time" />
-          </Form.Group>
-          <Form.Group className="row mb-3 col-md-8 " controlId="locationURL">
-            <Form.Label>Google Map URL</Form.Label>
-            <Form.Control type="text" placeholder="Enter URL" />
+            <Form.Control
+              onChange={(e) => {
+                // setOpenTime(e.target.value)
+                setLocation({ ...location, openTime: e.target.value })
+              }}
+              type="time"
+            />
           </Form.Group>
 
           <Form.Group
@@ -48,62 +133,60 @@ export default function ManageLocation() {
             controlId="locationCloseTime"
           >
             <Form.Label>Close Time</Form.Label>
-            <Form.Control type="time" />
+            <Form.Control
+              onChange={(e) => {
+                // setCloseTime(e.target.value)
+                setLocation({ ...location, closeTime: e.target.value })
+              }}
+              type="time"
+            />
           </Form.Group>
-          <Form.Group
-            className="row mb-3 col-md-8 "
-            controlId="locationAvailableDays"
-          >
-            {/* can choose multiple days */}
-            <div className="row">
-              <Form.Label>Select Available Days</Form.Label>
-              <Form.Check
-                type="checkbox"
-                id="locationOpenMonday"
-                label="Monday"
-                className="col-sm-3"
-              />
-              <Form.Check
-                type="checkbox"
-                id="locationOpenTuesday"
-                label="Tuesday"
-                className="col-sm-3"
-              />
-              <Form.Check
-                type="checkbox"
-                id="locationOpenWednesday"
-                label="Wednesday"
-                className="col-sm-3"
-              />
-              <Form.Check
-                type="checkbox"
-                id="locationOpenThursday"
-                label="Thursday"
-                className="col-sm-3"
-              />
-              <Form.Check
-                type="checkbox"
-                id="locationOpenFriday"
-                label="Friday"
-                className="col-sm-3"
-              />
-              <Form.Check
-                type="checkbox"
-                id="locationOpenSaturday"
-                label="Saturday"
-                className="col-sm-3"
-              />
-              <Form.Check
-                type="checkbox"
-                id="locationOpenSunday"
-                label="Sunday"
-                className="col-sm-3"
-              />
+          <Form.Group className="col-md-8">
+            <div className="row ">
+              {[
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+                'Sunday',
+              ].map((type) => (
+                <div key={`${type}`} className="col-md-6">
+                  <Form.Check
+                    // type={type}
+                    id={`${type}`}
+                    label={`${type}`}
+                    onChange={(e) => {
+                      if (e.target.checked) {
+                        setLocation({
+                          ...location,
+                          availableDay: [...location.availableDay, type],
+                        })
+                      } else {
+                        setLocation({
+                          ...location,
+                          availableDay: location.availableDay.filter(
+                            (day) => day !== type,
+                          ),
+                        })
+                      }
+                    }}
+                  />
+                </div>
+              ))}
             </div>
           </Form.Group>
-          <Button variant="dark" type="submit" className="primary col-md-8 pb">
-            Submit
-          </Button>
+          <div className="d-flex align-items-center justify-content-center col-md-8 pb-3 pt-3">
+            <Button
+              variant="dark"
+              type="submit"
+              className="primary col-md-8 pb"
+              size="lg"
+            >
+              Submit
+            </Button>
+          </div>
         </Form>
       </div>
     </div>
