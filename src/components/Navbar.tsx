4c7f079579a5fad3fe7abec7b307e1dc5e2f4198
Sorawit <6330528521@student.chula.ac.th>
2023-02-16 00:21:20 +0700

    edit some layout, create dropdown button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,63 @@
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import { Link, Outlet } from 'react-router-dom'
-
+import {Dropdown} from 'react-bootstrap';
 export default function MyNavbar() {
   return (
+    
     <>
-      <Navbar expand="lg" bg="light">
+      <Navbar expand="lg"  className = "nav-bar">
         <Container>
           <Navbar.Brand>
-            <Link className="nav-link" to="/">
-              BookBix
+            <Link className="nav-link" to="/" >
+              <p className = "bookbix-logo-org">BookBix<sup className = "sup-script">org</sup></p>
             </Link>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Link className="nav-link" to="/profile">
-                Profile
-              </Link>
-              <Link className="nav-link" to="/register">
-                Register
-              </Link>
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Outlet />
-    </>
+          <Dropdown className='dropdown' >
+        <Dropdown.Toggle id="dropdown-autoclose-true">
+          Sorawit
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item>
+          <Link className="nav-link" to="/profile">
+            Profile
+          </Link>
+          </Dropdown.Item>
+          <Dropdown.Item>
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+          </Dropdown.Item>
+          <Dropdown.Item>
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+          </Dropdown.Item>
+          <Dropdown.Item>
+          <Link className="nav-link" to="/logout">
+            Logout
+          </Link>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+           {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
+           <Navbar.Collapse id="basic-navbar-nav">
+             <Nav className="ms-auto">
+               <Link className="nav-link" to="/profile">
+                 Profile
+               </Link>
+               <Link className="nav-link" to="/register">
+                 Register
+               </Link>
+               <Link className="nav-link" to="/login">
+                 Login
+               </Link>
+             </Nav>
+           </Navbar.Collapse> */}
+         </Container>
+
+       </Navbar>
+       <Outlet />
+     </>
   )
 }
