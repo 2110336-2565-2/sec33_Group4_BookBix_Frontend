94e88624be8584834f68cb5e2e3cfddc189e9c5e
chatreeck7 <chatree.devl@gmail.com>
2023-02-23 16:04:07 +0700

    ➕ Add credential when i fetch the POST API to save cookie in the client

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Home from '../screens/Home'
@@ -7,8 +8,33 @@ import BookLocation from '../screens/BookLocation'
 import Register from '../screens/Register'
 import ManageProfile from '../screens/ManageProfile'
 import ManageLocation from '../screens/provider/ManageLocation'
+import { UserInterface } from '../interfaces/user.interfaces'
+
+const URL = import.meta.env.VITE_API_URL
 
 function App() {
+  const [user, setUser] = useState<UserInterface | null>(null)
+
+  useEffect(() => {
+    // fetch to get user info
+    // if user is logged in, set user info in context
+    async function getUserInfo() {
+      const response = await fetch(`${URL}/users/me`, {
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+      })
+
+      const data = await response.json()
+
+      if (response.ok) {
+        setUser(data)
+        console.log(data)
+      }
+    }
+
+
+  },[])
+
   return (
     <>
       <Routes>
