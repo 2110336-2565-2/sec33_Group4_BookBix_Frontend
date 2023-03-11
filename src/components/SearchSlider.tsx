89ec02b936a7540663e2f4096028c065dac41076
Ham <6330104021@student.chula.ac.th>
2023-03-11 19:50:08 +0700

    Add location cards

---
+++ src/components/SearchSlider.tsx
@@ -0,0 +1,207 @@
+import { useState } from 'react'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+export const SearchForm = () => {
+  const [location, setLocation] = useState<string>('')
+  const handleSummit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // search location by name
+    try {
+      const response = fetch(`${URL}locations/search?location=${location}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <div>
+      <h1>Let us help you find your</h1>
+      <Form onSubmit={handleSummit}>
+        <Row>
+          <Form.Group controlId="formLocationName">
+            <Form.Control
+              onChange={(e) => {
+                setLocation(e.target.value)
+              }}
+              type="text"
+              placeholder="Enter location"
+            />
+          </Form.Group>
+        </Row>
+        <Row className="pt-3">
+          <Col>
+            <Form.Group controlId="formLocationType">
+              <Form.Control as="select">
+                <option>Choose location type</option>
+                <option>Hotel</option>
+                <option>Restaurant</option>
+                <option>Bar</option>
+                <option>Other</option>
+              </Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="formLocationFunction">
+              <Form.Control as="select">
+                <option>Choose location function</option>
+                <option>Wedding</option>
+                <option>Party</option>
+                <option>Meeting</option>
+                <option>Other</option>
+              </Form.Control>
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  )
+}
+
+export const PriceSlider = () => {
+  const [value, setValue] = useState<{ min: number; max: number }>({ min: 10, max: 1000 })
+  // Create a range slider
+  const fromSlider = document.querySelector('#fromSlider') as HTMLInputElement
+  const toSlider = document.querySelector('#toSlider') as HTMLInputElement
+  const fromInput = document.querySelector('#fromInput') as HTMLInputElement
+  const toInput = document.querySelector('#toInput') as HTMLInputElement
+
+  const controlSlider = document.querySelector('.sliders_control')
+
+  function controlFromInput(e: React.FormEvent) {
+    const eventTarget = e.target as HTMLInputElement
+    const from: number = Math.max(Number(eventTarget.value), 0)
+    const to = value.max
+    fillSlider('#C6C6C6', '#25daa5')
+    if (from > to) {
+      setValue({ min: to, max: to })
+    } else {
+      setValue({ min: from, max: to })
+    }
+  }
+
+  function controlToInput(e: React.FormEvent) {
+    const eventTarget = e.target as HTMLInputElement
+    const from = value.min
+    const to: number = Math.min(Number(eventTarget.value), 1000)
+    fillSlider('#C6C6C6', '#25daa5')
+    setToggleAccessible(e)
+    if (from <= to) {
+      setValue({ min: from, max: to })
+    } else {
+      setValue({ min: from, max: from })
+    }
+  }
+
+  function controlFromSlider(e: React.FormEvent) {
+    const eventTarget = e.target as HTMLInputElement
+    const from: number = Math.max(Number(eventTarget.value), 0)
+    const to = value.max
+    fillSlider('#C6C6C6', '#25daa5')
+    if (from > to) {
+      setValue({ min: to, max: to })
+    } else {
+      setValue({ min: from, max: to })
+    }
+  }
+
+  function controlToSlider(e: React.FormEvent) {
+    const from = value.min
+    const eventTarget = e.target as HTMLInputElement
+    const to: number = Math.min(Number(eventTarget.value), 1000)
+    fillSlider('#C6C6C6', '#25daa5')
+    setToggleAccessible(e)
+    if (from <= to) {
+      setValue({ min: from, max: to })
+    } else {
+      setValue({ min: from, max: from })
+    }
+  }
+  function fillSlider(sliderColor: string, rangeColor: string) {
+    const from = value.min
+    const to = value.max
+    //
+    //Default function if needed,you can change the constant variables
+    //
+    const rangeDistance = 100
+    const fromPosition = from
+    const toPosition = to
+    // controlSlider.style.background = `linear-gradient(
+    //   to right,
+    //   ${sliderColor} 0%,
+    //   ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
+    //   ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
+    //   ${rangeColor} ${(toPosition / rangeDistance) * 100}%,
+    //   ${sliderColor} ${(toPosition / rangeDistance) * 100}%,
+    //   ${sliderColor} 100%)`
+  }
+
+  function setToggleAccessible(e?: React.FormEvent) {
+    //
+    //I don't know what this function actually do than I will skip this function
+    //
+    if (e) {
+      // console.log(e.target.value)
+    }
+    // if (Number(currentTarget.value) <= 0) {
+    //   toSlider.style.zIndex = '2'
+    // } else {
+    //   toSlider.style.zIndex = '0'
+    // }
+  }
+
+  fillSlider('#C6C6C6', '#25daa5')
+  setToggleAccessible()
+  return (
+    <div className="range_container">
+      <div className="sliders_control">
+        <h1>Your budget (per hour)</h1>
+        <input
+          id="fromSlider"
+          type="range"
+          value={value.min}
+          min="0"
+          max="1000"
+          onInput={(e: React.FormEvent) => controlFromSlider(e)}
+        />
+        <input
+          id="toSlider"
+          type="range"
+          value={value.max}
+          min="0"
+          max="1000"
+          onInput={(e: React.FormEvent) => controlToSlider(e)}
+        />
+      </div>
+      <div className="form_control">
+        <div className="form_control_container">
+          <div className="form_control_container__time">Min</div>
+          <input
+            className="form_control_container__time__input"
+            type="number"
+            id="fromInput"
+            value={value.min}
+            min="0"
+            max="1000"
+            onInput={(e: React.FormEvent) => controlFromInput(e)}
+          />
+        </div>
+        <div className="form_control_container">
+          <div className="form_control_container__time">Max</div>
+          <input
+            className="form_control_container__time__input"
+            type="number"
+            id="toInput"
+            value={value.max}
+            min="0"
+            max="1000"
+            onInput={(e: React.FormEvent) => controlToInput(e)}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/screens/customer/Search.tsx
+++ src/screens/customer/Search.tsx
@@ -1,221 +1,30 @@
 import React from 'react'
-import { searchForm } from '../../components/SearchPage'
-import { Button, Form, Row } from 'react-bootstrap'
+import { SearchForm, PriceSlider } from '../../components/SearchSlider'
+import { SearchCard } from '../../components/SearchCard'
+import { Button, Form, Row, Col, Container } from 'react-bootstrap'
 import { useState } from 'react'
 
-export default function SearchPage() {
-  const url = import.meta.env.VITE_API_URL
-  const [location, setLocation] = useState<string>('')
+const URL = import.meta.env.VITE_API_URL
 
-  // handle submit
-  const handleSummit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    // console.log(location)
-    // search location by name
-    try {
-      const response = fetch(`${url}locations/search?location=${location}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      console.log(response)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  // slider
-
-  function controlFromInput(
-    fromSlider: HTMLInputElement,
-    fromInput: HTMLInputElement,
-    toInput: HTMLInputElement,
-    controlSlider: HTMLInputElement,
-  ) {
-    const [from, to] = getParsed(fromInput, toInput)
-    fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider)
-    if (from > to) {
-      fromSlider.value = to.toString()
-      fromInput.value = to.toString()
-    } else {
-      fromSlider.value = from.toString()
-    }
-  }
-
-  function controlToInput(
-    toSlider: HTMLInputElement,
-    fromInput: HTMLInputElement,
-    toInput: HTMLInputElement,
-    controlSlider: HTMLInputElement,
-  ) {
-    const [from, to] = getParsed(fromInput, toInput)
-    fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider)
-    setToggleAccessible(toInput)
-    if (from <= to) {
-      toSlider.value = to.toString()
-      toInput.value = to.toString()
-    } else {
-      toInput.value = from.toString()
-    }
-  }
-
-  function controlFromSlider(
-    fromSlider: HTMLInputElement,
-    toSlider: HTMLInputElement,
-    fromInput: HTMLInputElement,
-  ) {
-    const [from, to] = getParsed(fromSlider, toSlider)
-    fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider)
-    if (from > to) {
-      fromSlider.setAttribute('value', to.toString())
-      fromInput.setAttribute('value', to.toString())
-    } else {
-      fromInput.setAttribute('value', from.toString())
-    }
-  }
-
-  function controlToSlider(
-    fromSlider: HTMLInputElement,
-    toSlider: HTMLInputElement,
-    toInput: HTMLInputElement,
-  ) {
-    const [from, to] = getParsed(fromSlider, toSlider)
-    fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider)
-    setToggleAccessible(toSlider)
-    if (from <= to) {
-      toSlider.value = to.toString()
-      toInput.value = to.toString()
-    } else {
-      toInput.value = from.toString()
-      toSlider.value = from.toString()
-    }
-  }
-
-  function getParsed(
-    currentFrom: HTMLInputElement,
-    currentTo: HTMLInputElement,
-  ) {
-    const from = parseInt(currentFrom.value, 10)
-    const to = parseInt(currentTo.value, 10)
-    return [from, to]
-  }
-
-  function fillSlider(
-    from: HTMLInputElement,
-    to: HTMLInputElement,
-    sliderColor: string,
-    rangeColor: string,
-    controlSlider: HTMLInputElement,
-  ) {
-    console.log(
-      from.getAttribute('value')?.valueOf(),
-      to.getAttribute('min')?.valueOf(),
-      sliderColor,
-      rangeColor,
-      controlSlider,
-    )
-    const rangeDistance =
-      Number(to.getAttribute('max')?.valueOf()) -
-      Number(to.getAttribute('min')?.valueOf())
-    const fromPosition =
-      Number(from.getAttribute('value')?.valueOf()) -
-      Number(to.getAttribute('min')?.valueOf())
-    const toPosition =
-      Number(to.getAttribute('value')?.valueOf()) -
-      Number(to.getAttribute('min')?.valueOf())
-    controlSlider.style.background = `linear-gradient(
-      to right,
-      ${sliderColor} 0%,
-      ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
-      ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
-      ${rangeColor} ${(toPosition / rangeDistance) * 100}%,
-      ${sliderColor} ${(toPosition / rangeDistance) * 100}%,
-      ${sliderColor} 100%)`
-  }
-
-  function setToggleAccessible(currentTarget: HTMLInputElement) {
-    const toSlider = document.querySelector('#toSlider') as HTMLInputElement
-    if (Number(currentTarget.value) <= 0) {
-      toSlider.style.zIndex = '2'
-    } else {
-      toSlider.style.zIndex = '0'
-    }
-  }
-  const fromSlider = document.querySelector('#fromSlider') as HTMLInputElement
-  const toSlider = document.querySelector('#toSlider') as HTMLInputElement
-  const fromInput = document.querySelector('#fromInput') as HTMLInputElement
-  const toInput = document.querySelector('#toInput') as HTMLInputElement
-  fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider)
-  setToggleAccessible(toSlider)
-  fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput)
-  toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput)
-  fromInput.oninput = () =>
-    controlFromInput(fromSlider, fromInput, toInput, toSlider)
-  toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider)
+const SearchPage: React.FC = () => {
   return (
-    <div className="search-page">
-      <div className="fill bg-dark  justify-content-center d-flex pt-5">
-        <div>
-          <h1>Let us help you find your</h1>
-          <Form onSubmit={handleSummit}>
-            <Form.Group controlId="formLocationName">
-              <Form.Control
-                onChange={(e) => {
-                  setLocation(e.target.value)
-                }}
-                type="text"
-                placeholder="Enter location"
-              />
-            </Form.Group>
-            <div className="range_container">
-              <div className="sliders_control">
-                <input
-                  id="fromSlider"
-                  type="range"
-                  value="10"
-                  min="0"
-                  max="100"
-                />
-                <input
-                  id="toSlider"
-                  type="range"
-                  value="40"
-                  min="0"
-                  max="100"
-                />
-              </div>
-              <div className="form_control">
-                <div className="form_control_container">
-                  <div className="form_control_container__time">Min</div>
-                  <input
-                    className="form_control_container__time__input"
-                    type="number"
-                    id="fromInput"
-                    value="10"
-                    min="0"
-                    max="100"
-                  />
-                </div>
-                <div className="form_control_container">
-                  <div className="form_control_container__time">Max</div>
-                  <input
-                    className="form_control_container__time__input"
-                    type="number"
-                    id="toInput"
-                    value="40"
-                    min="0"
-                    max="100"
-                  />
-                </div>
-              </div>
-            </div>
-            <Button type="submit" className="search-btn submit">
-              Search
-            </Button>
-          </Form>
+    <Container fluid className="search-page bg-dark">
+      <div className="align-content-center fill">
+        <Row className="pt-5 to_center">
+          <SearchForm />
+        </Row>
+        <Row className="p-3 to_center">
+          <PriceSlider />
+        </Row>
+        <Button variant="primary" type="submit" className="search-btn to_center">
+          Search
+        </Button>
+        <div className="to_center">
+          <SearchCard />
         </div>
       </div>
-    </div>
+    </Container>
   )
 }
+
+export default SearchPage
