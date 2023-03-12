8b4efb2ee5be7d9d0c914e6d3bb8953d85276738
Nick <nick.rattanaton@gmail.com>
2023-03-12 16:57:15 +0700

    🌹Fixed Some old redundant code and make more readable

--- src/utils/Booking.utils.tsx
+++ src/utils/Booking.utils.tsx
@@ -1,33 +1,52 @@
 import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-export function statusSelector(status: string, component: string, locationId: string) {
+export enum ComponentType {
+  PROGRESS_CIRCLE = 'progress-circle',
+  ACTION_BUTTON = 'action-btn',
+}
+export enum StatusType {
+  BUTTON_PENDING = 'pending',
+  BUTTON_CANCEL = 'canceled',
+  BUTTON_CONFIRMED = 'confirmed',
+}
+interface StatusSelectorInterface {
+  status: string
+  component: string
+  locationId: string
+}
+
+export const StatusSelector = ({ status, component, locationId }: StatusSelectorInterface) => {
   switch (component) {
-    case 'progress-circle':
+    case ComponentType.PROGRESS_CIRCLE:
       return (
-        <div className={`outer-progress-circle ${status}`}>
-          <div className="inner-progress-circle"></div>
+        <div className="progress-circle d-flex flex-column justify-content-center align-items-center">
+          <div className={`status-text ${status}`}>{status}</div>
+          <div className={`outer-progress-circle ${status}`}>
+            <div className="inner-progress-circle"></div>
+          </div>
         </div>
       )
-    case 'status-text':
-      return <div className={`status-text ${status}`}>{status}</div>
-    case 'action-btn':
-      if (status === 'canceled') {
-        return <div></div>
-      } else if (status === 'pending') {
-        return (
-          <Link to={`/payment`}>
-            <Button className={`action-btn ${status} text-dark`}>Payment</Button>
-          </Link>
-        )
-      } else if (status === 'confirmed') {
-        return (
-          <Link to={`/location/${locationId}/review`}>
+    case ComponentType.ACTION_BUTTON:
+      switch (status) {
+        case StatusType.BUTTON_CANCEL:
+          return <div></div>
+        case StatusType.BUTTON_PENDING:
+          return (
+            <Link to={`/payment`}>
+              <Button className={`action-btn ${status} text-dark`}>Payment</Button>
+            </Link>
+          )
+        case StatusType.BUTTON_CONFIRMED:
+          return (
+            // <Link to={`/location/${locationId}/review`}>
             <Button className={`action-btn ${status} text-dark`}>Review</Button>
-          </Link>
-        )
+            // </Link>
+          )
+        default:
+          return <></>
       }
     default:
-      return ''
+      return <></>
   }
 }
