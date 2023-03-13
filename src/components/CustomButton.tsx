8e8d5a9c705438d7d7a60162b4734d6c077dc4a7
Nick <nick.rattanaton@gmail.com>
2023-03-13 22:54:03 +0700

    🐱‍👤 Add jwt-decode package

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,23 +1,34 @@
 import { useState } from 'react'
 import { Button } from 'react-bootstrap'
-import { ReviewModal } from './CustomModal'
+import { ReviewModal, reviewRespondInterface } from './CustomModal'
+import jwt_decode from 'jwt-decode'
 
 export const ButtonReview: React.FC<{ locationId: string }> = ({ locationId }) => {
   const [show, setShow] = useState<boolean>(false)
-  const [rating, setRating] = useState<number>(0)
+  const [reviewRespond, setReviewRespond] = useState<reviewRespondInterface>({
+    title: undefined,
+    locationId: locationId,
+    rating: 0,
+    review: undefined,
+  })
 
   const reviewButtonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
+
+    var token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MGYzZDFlZTZhZDE1ZmI4N2JiOTZlZiIsImVtYWlsIjoia3V5YWlAbWFpbC5jb20iLCJ0eXBlIjoiY3VzdG9tZXIiLCJpYXQiOjE2Nzg3MjAzNDcsImV4cCI6MTY3ODcyMDQwN30.FnR2QHtqnR0QfAumAtO1t_WRH5CRIe-1zx66e2Q6raM'
+    var decodedHeader = jwt_decode(token)
+    console.log(decodedHeader)
+
     setShow(true)
-    console.log('reviewHandler')
-    console.log(locationId)
+    console.log('Open', reviewRespond.locationId)
   }
   const handleCancel = () => {
-    console.log('Cancel Review')
     setShow(false)
+    console.log('Close', reviewRespond.locationId)
   }
   const handleSubmit = () => {
-    console.log('Sent Review')
+    console.log('Submit', reviewRespond)
     setShow(false)
   }
 
@@ -26,7 +37,13 @@ export const ButtonReview: React.FC<{ locationId: string }> = ({ locationId }) =
       <Button onClick={reviewButtonHandler} className="action-btn confirmed text-dark">
         Review
       </Button>
-      <ReviewModal show={show} locationId={locationId} handleCancel={handleCancel} handleSubmit={handleSubmit} />
+      <ReviewModal
+        show={show}
+        reviewRespond={reviewRespond}
+        setReviewRespond={setReviewRespond}
+        handleCancel={handleCancel}
+        handleSubmit={handleSubmit}
+      />
     </>
   )
 }
