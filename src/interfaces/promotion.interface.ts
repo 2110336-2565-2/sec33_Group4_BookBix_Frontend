ed3435540f77830ddad3a7ccc8e7f710d60e5540
Sorawit <6330528521@student.chula.ac.th>
2023-03-31 13:17:03 +0700

    adjust discount form from discount type

---
+++ src/interfaces/promotion.interface.ts
@@ -0,0 +1,7 @@
+export interface PromotionInterface {
+  name: string
+  percentOff: string | null
+  amountOff: string | null
+  locationName: string
+  maxRedemptions: string
+}
--- src/screens/provider/CreatePromotion.tsx
+++ src/screens/provider/CreatePromotion.tsx
@@ -1,45 +1,98 @@
 import React from 'react'
 import { Button, Form, Row } from 'react-bootstrap'
 import { useState } from 'react'
+import { PromotionInterface } from '../../interfaces/promotion.interface'
 const url = import.meta.env.VITE_API_URL
 
-// page for update user info firstname, lastname, sex, birthdate, email
+// page for update user info firstname, lastname, sex, birthdate, email]
+
+// const DiscountForm: React.FC<>{} = ({ discountType = '', setPromotion = () => {} }) => {
+//   console.log(discountType)
+//   if (discountType == 'Percentage') {
+//     return (
+//       <>
+//         <Form.Label>Percentage</Form.Label>
+//         <Form.Control
+//           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+//             setPromotion({ ...promotion, amountOff: null, percentOff: e.target.value })
+//           }
+//           type="text"
+//           placeholder="Enter Percentage to discount"
+//         />
+//       </>
+//     )
+//   }
+//   return (
+//     <>
+//       <Form.Label>Amount of Discount</Form.Label>
+//       <Form.Control
+//         onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+//           setPromotion({ ...promotion, amountOff: e.target.value, percentOff: null })
+//         }
+//         type="text"
+//         placeholder="Enter Amount of Discount"
+//       />
+//     </>
+//   )
+// }
+
 export default function CreatePromotion() {
-  interface User {
-    PromotionCode: string
-    Discount: string
-    AmountOfDiscount: string
-    Location: string
-    MaxRedemptions: string
-  }
   const [error, setError] = useState<string>('')
-  const [user, setUser] = useState<User>({
-    PromotionCode: '',
-    Discount: '',
-    AmountOfDiscount: '',
-    Location: '',
-    MaxRedemptions: '',
+  const [promotion, setPromotion] = useState<PromotionInterface>({
+    name: '',
+    percentOff: '',
+    amountOff: '',
+    locationName: '',
+    maxRedemptions: '',
   })
+
+  const [discountType, setDiscountType] = useState<string>('')
+
+  const discountForm = (discountType: string) => {
+    console.log(discountType)
+    if (discountType == 'Percentage') {
+      return (
+        <>
+          <Form.Label>Percentage</Form.Label>
+          <Form.Control
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPromotion({ ...promotion, amountOff: null, percentOff: e.target.value })
+            }
+            type="text"
+            placeholder="Enter Percentage to discount"
+          />
+        </>
+      )
+    }
+    return (
+      <>
+        <Form.Label>Amount of Discount</Form.Label>
+        <Form.Control
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPromotion({ ...promotion, amountOff: e.target.value, percentOff: null })
+          }
+          type="text"
+          placeholder="Enter Amount of Discount"
+        />
+      </>
+    )
+  }
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     try {
       const response = await fetch(`${url}/stripe/create-promotion`, {
-        method: 'PUT',
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(user),
+        body: JSON.stringify(promotion),
       })
       const data = await response.json()
       if (!response.ok) {
         setError(data.message)
         return
       }
-      // Save the user information in local storage or in the state
-      localStorage.setItem('user', JSON.stringify(data.user))
-
-      // Redirect the user to the homepage
-      window.location.href = '/home'
     } catch (error) {
       setError('Something went wrong, please try again later')
     }
@@ -53,55 +106,65 @@ export default function CreatePromotion() {
             <h1 className=" row mb-3 col-md-8 justify-content-center  d-flex align-items-center justify-content-center">
               Create Promotion
             </h1>
-            <Form.Group className=" row mb-3 col-md-8 justify-content-center  justify-content-center " controlId="formPromotionCode">
+            <Form.Group
+              className=" row mb-3 col-md-8 justify-content-center  justify-content-center "
+              controlId="formPromotionCode"
+            >
               <Form.Label>Promotion Code</Form.Label>
               <Form.Control
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUser({ ...user, PromotionCode: e.target.value })}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPromotion({ ...promotion, name: e.target.value })
+                }
                 type="text"
                 placeholder="Enter Promotion Code"
               />
             </Form.Group>
 
-            
-            
-            <Form.Group className=" row mb-3 col-md-8 justify-content-center "  controlId="formDiscount">
+            <Form.Group className=" row mb-3 col-md-8 justify-content-center " controlId="formDiscount">
               <Form.Label>Discount</Form.Label>
               <Form.Select
                 onChange={(e) => {
-                  setUser({ ...user, Discount: e.target.value })
+                  setDiscountType(e.target.value)
                 }}
                 aria-label="Default select example"
                 placeholder="Enter Discount Type"
               >
                 <option value="">Choose...</option>
                 <option value="Baht">Baht</option>
-                <option value="Percantage">Percentage</option>
+                <option value="Percentage">Percentage</option>
               </Form.Select>
             </Form.Group>
-            
-            <Form.Group className=" row mb-3 col-md-8 justify-content-center justify-content-center" controlId="formLastname">
-              <Form.Label>Amount of Discount</Form.Label>
-              <Form.Control
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUser({ ...user, AmountOfDiscount: e.target.value })}
-                type="text"
-                placeholder="Enter Amount of Discount"
-              />
-            </Form.Group>
-            
 
-            <Form.Group className=" row mb-3 col-md-8 justify-content-center justify-content-center " controlId="formLocation">
+            <Form.Group
+              className=" row mb-3 col-md-8 justify-content-center justify-content-center"
+              controlId="formDiscountType"
+            >
+              {discountForm(discountType)}
+            </Form.Group>
+
+            <Form.Group
+              className=" row mb-3 col-md-8 justify-content-center justify-content-center "
+              controlId="formLocation"
+            >
               <Form.Label>Location</Form.Label>
               <Form.Control
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUser({ ...user, Location: e.target.value })}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPromotion({ ...promotion, locationName: e.target.value })
+                }
                 type="text"
                 placeholder="Enter Location"
               />
             </Form.Group>
 
-            <Form.Group className=" row mb-3 col-md-8 justify-content-center justify-content-center  " controlId="formMaxRedemptions">
+            <Form.Group
+              className=" row mb-3 col-md-8 justify-content-center justify-content-center  "
+              controlId="formMaxRedemptions"
+            >
               <Form.Label>Max Redemptions</Form.Label>
               <Form.Control
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUser({ ...user, MaxRedemptions: e.target.value })}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPromotion({ ...promotion, maxRedemptions: e.target.value })
+                }
                 type="text"
                 placeholder="Enter Max Redemptions"
               />
