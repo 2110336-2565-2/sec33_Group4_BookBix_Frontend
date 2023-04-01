6017e2f6f9873292a8a80a07ef01145f5868f414
S1xZ <punya.gnwdn@gmail.com>
2023-04-01 17:40:49 +0700

    Fix docker image remove command no longer working
    💃change all the docker file related to the problems above

--- dockerfront.sh
+++ dockerfront.sh
@@ -1,2 +1,3 @@
 docker-compose down -v
-docker-compose -f docker-compose.front.yml up -d --build && docker rmi $(docker images -f dangling=true -q)!
+docker-compose -f docker-compose.front.yml up -d --build
+docker image prune -af
