8a5b1db405cba4cd841557bcc30ab03566a41b22
Nick <nick.rattanaton@gmail.com>
2023-04-01 09:42:09 +0700

    🎒🌹 Implement first version with mock data

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -12,17 +12,151 @@ const LocationCard: React.FC<locationInterface> = ({
 }) => {
   return (
     <>
-      <div>
-        <h4>{name}</h4>
-        <h4>{address}</h4>
-        <h4>{description}</h4>
-        <h4>{url}</h4>
-        <h4>{images}</h4>
-        <h4>{time.open_time}</h4>
-        <h4>{time.close_time}</h4>
-        <h4>{available_days}</h4>
-        <h4>{price}</h4>
-      </div>
+      <article className="postcard dark blue">
+        <a className="postcard__img_link" href={url}>
+          <img className="postcard__img" src="https://picsum.photos/1000/1000" alt={`${name} Image`} />
+        </a>
+        <div className="postcard__text">
+          <h1 className="postcard__title blue">
+            <a href="#">{name}</a>
+          </h1>
+          <div className="postcard__subtitle small">
+            {address}
+            {/* <time dateTime="2020-05-25 12:00:00">
+              <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+            </time> */}
+          </div>
+          <div className="postcard__bar"></div>
+          <div className="postcard__preview-txt">{description}</div>
+          <ul className="postcard__tagbox">
+            <li className="tag__item">
+              <i className="fas fa-tag mr-2"></i>
+              {price} &#3647;
+            </li>
+            {available_days.map((day, idx) => {
+              return (
+                <li key={idx} className="tag__item">
+                  <i className="fas fa-clock mr-2"></i>
+                  {day}
+                </li>
+              )
+            })}
+            <li className="tag__item play blue">
+              <a href="#">
+                <i className="fas fa-play mr-2"></i>Play Episode
+              </a>
+            </li>
+          </ul>
+        </div>
+      </article>
+
+      {/* <article className="postcard dark red">
+        <a className="postcard__img_link" href="#">
+          <img className="postcard__img" src="https://picsum.photos/501/500" alt="Image Title" />
+        </a>
+        <div className="postcard__text">
+          <h1 className="postcard__title red">
+            <a href="#">Podcast Title</a>
+          </h1>
+          <div className="postcard__subtitle small">
+            <time dateTime="2020-05-25 12:00:00">
+              <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+            </time>
+          </div>
+          <div className="postcard__bar"></div>
+          <div className="postcard__preview-txt">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae
+            accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti
+            tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla
+            unde ipsum dolores nobis enim quidem excepturi, illum quos!
+          </div>
+          <ul className="postcard__tagbox">
+            <li className="tag__item">
+              <i className="fas fa-tag mr-2"></i>Podcast
+            </li>
+            <li className="tag__item">
+              <i className="fas fa-clock mr-2"></i>55 mins.
+            </li>
+            <li className="tag__item play red">
+              <a href="#">
+                <i className="fas fa-play mr-2"></i>Play Episode
+              </a>
+            </li>
+          </ul>
+        </div>
+      </article>
+
+      <article className="postcard dark green">
+        <a className="postcard__img_link" href="#">
+          <img className="postcard__img" src="https://picsum.photos/500/501" alt="Image Title" />
+        </a>
+        <div className="postcard__text">
+          <h1 className="postcard__title green">
+            <a href="#">Podcast Title</a>
+          </h1>
+          <div className="postcard__subtitle small">
+            <time dateTime="2020-05-25 12:00:00">
+              <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+            </time>
+          </div>
+          <div className="postcard__bar"></div>
+          <div className="postcard__preview-txt">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae
+            accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti
+            tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla
+            unde ipsum dolores nobis enim quidem excepturi, illum quos!
+          </div>
+          <ul className="postcard__tagbox">
+            <li className="tag__item">
+              <i className="fas fa-tag mr-2"></i>Podcast
+            </li>
+            <li className="tag__item">
+              <i className="fas fa-clock mr-2"></i>55 mins.
+            </li>
+            <li className="tag__item play green">
+              <a href="#">
+                <i className="fas fa-play mr-2"></i>Play Episode
+              </a>
+            </li>
+          </ul>
+        </div>
+      </article>
+
+      <article className="postcard dark yellow">
+        <a className="postcard__img_link" href="#">
+          <img className="postcard__img" src="https://picsum.photos/501/501" alt="Image Title" />
+        </a>
+        <div className="postcard__text">
+          <h1 className="postcard__title yellow">
+            <a href="#">Podcast Title</a>
+          </h1>
+          <div className="postcard__subtitle small">
+            <time dateTime="2020-05-25 12:00:00">
+              <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+            </time>
+          </div>
+          <div className="postcard__bar"></div>
+          <div className="postcard__preview-txt">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae
+            accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti
+            tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla
+            unde ipsum dolores nobis enim quidem excepturi, illum quos!
+          </div>
+          <ul className="postcard__tagbox">
+            <li className="tag__item">
+              <i className="fas fa-tag mr-2"></i>Podcast
+            </li>
+            <li className="tag__item">
+              <i className="fas fa-clock mr-2"></i>55 mins.
+            </li>
+            <li className="tag__item play yellow">
+              <a href="#">
+                <i className="fas fa-play mr-2"></i>Play Episode
+              </a>
+            </li>
+          </ul>
+        </div>
+      </article> */}
     </>
   )
 }
--- src/screens/provider/LocationsManage.tsx
+++ src/screens/provider/LocationsManage.tsx
@@ -1,5 +1,6 @@
 import LocationCard from '../../components/CustomCard'
 import { locationInterface } from '../../interfaces/location.interfaces'
+import { Container } from 'react-bootstrap'
 
 export default function LocationsManage() {
   const testLocations: locationInterface[] = [
@@ -20,41 +21,59 @@ export default function LocationsManage() {
       available_days: ['Monday', 'Wednesday', 'Friday'],
       price: 200,
     },
-    // {
-    //   name: 'Eiffel Tower',
-    //   address: 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France',
-    //   description:
-    //     "The Eiffel Tower is an iron tower located on the Champ de Mars in Paris, France. It was built as the entrance arch to the 1889 World's Fair.",
-    //   url: 'test',
-    //   images: [
-    //     'https://www.travelandleisure.com/thmb/SPUPzO88ZXq6P4Sm4mC5Xuinoik=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/eiffel-tower-paris-france-EIFFEL0217-6ccc3553e98946f18c893018d5b42bde.jpg',
-    //     'https://media.istockphoto.com/photos/eiffel-tower-in-spring-picture-id1297043676?b=1&k=20&m=1297043676&s=170667a&w=0&h=kXklVKUeWIw7UWDDYnxMzB3uXS9prFiea3RaRPyB5M0=',
-    //   ],
-    //   time: { open_time: '08:00', close_time: '16:00' },
-    //   available_days: ['Monday', 'Wednesday', 'Friday', 'Sunday'],
-    //   price: 200,
-    // },
+    {
+      name: 'Eiffel Tower',
+      address: 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France',
+      description:
+        "The Eiffel Tower is an iron tower located on the Champ de Mars in Paris, France. It was built as the entrance arch to the 1889 World's Fair.",
+      url: 'test',
+      images: [
+        'https://www.travelandleisure.com/thmb/SPUPzO88ZXq6P4Sm4mC5Xuinoik=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/eiffel-tower-paris-france-EIFFEL0217-6ccc3553e98946f18c893018d5b42bde.jpg',
+        'https://media.istockphoto.com/photos/eiffel-tower-in-spring-picture-id1297043676?b=1&k=20&m=1297043676&s=170667a&w=0&h=kXklVKUeWIw7UWDDYnxMzB3uXS9prFiea3RaRPyB5M0=',
+      ],
+      time: { open_time: '08:00', close_time: '16:00' },
+      available_days: ['Monday', 'Wednesday', 'Friday', 'Sunday'],
+      price: 200,
+    },
+    {
+      name: 'Central Park',
+      address: 'New York, NY 10022',
+      description: 'A large urban park in the heart of New York City',
+      url: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.066385305769!2d-73.96777208496323!3d40.78255467932428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2589a018531e3%3A0xb9df1f7387a94119!2sCentral%20Park!5e0!3m2!1sen!2sth!4v1675496921164!5m2!1sen!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+      images: [
+        'https://ohny.org/wp-content/uploads/2022/09/Harry_Gillen_via_Unsplash.jpeg',
+        'https://mymodernmet.com/wp/wp-content/uploads/2020/12/central-park-new-york-city-frederick-law-olmsted-2.jpg',
+        'https://www.planetware.com/photos-large/USNY/new-york-city-central-park-1.jpg',
+      ],
+      time: { open_time: '10:00', close_time: '16:00' },
+      available_days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      price: 250,
+    },
   ]
   return (
     <>
-      <div>
-        <h2 className="">Your locations</h2>
-        {testLocations.map(({ name, address, description, url, images, time, available_days, price }) => {
-          return (
-            <LocationCard
-              key={name}
-              name={name}
-              address={address}
-              description={description}
-              url={url}
-              images={images}
-              time={time}
-              available_days={available_days}
-              price={price}
-            />
-          )
-        })}
-      </div>
+      <section className="location-manage bg-dark">
+        <Container className="py-4">
+          <h1 className="h1 text-center fw-bold" id="header">
+            Your locations
+          </h1>
+          {testLocations.map(({ name, address, description, url, images, time, available_days, price }, idx) => {
+            return (
+              <LocationCard
+                key={idx}
+                name={name}
+                address={address}
+                description={description}
+                url={url}
+                images={images}
+                time={time}
+                available_days={available_days}
+                price={price}
+              />
+            )
+          })}
+        </Container>
+      </section>
     </>
   )
 }
